4c8372fa6d1e62f811715e343de0dcbbd5fa4b04
Oksana <solomkao@gmail.com>
2021-02-16 21:37:07 +0200

    add eureka properties

--- src/main/java/com/solomka/eurekaserver/Homework20EurekaServerApplication.java
+++ src/main/java/com/solomka/eurekaserver/Homework20EurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.solomka.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class Homework20EurekaServerApplication {
 
     public static void main(String[] args) {
